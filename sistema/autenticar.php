3a20c18b97c00853a066a3ff295cec1c1c9c8978
abelnalima <abel.na.lima@gmail.com>
2021-04-15 08:53:06 -0300

    Identificando o tipo de usuário ao entrar com as credenciais.

--- sistema/autenticar.php
+++ sistema/autenticar.php
@@ -1,6 +1,6 @@
 <?php
-
     require_once("../conexao.php");
+    @session_start(); //@ para evitar que seja mostrado avisos de alerta dependendo da versão do PHP
 
     $email = $_POST['email_login'];
     $senha = md5($_POST['senha_login']);
@@ -9,6 +9,18 @@
     $dados = $result->fetchAll(PDO::FETCH_ASSOC);
 
     if (@count($dados) > 0) {
-        echo "Usuário Encontrado";
+        $_SESSION['nome_usuario'] = $dados[0]['nome'];  //session recebe o nome do primeiro usuario encontrado pela query
+        $_SESSION['email_usuario'] = $dados[0]['email'];
+        $_SESSION['cpf_usuario'] = $dados[0]['cpf'];
+        $_SESSION['nivel_usuario'] = $dados[0]['nivel'];
+        
+        if ($_SESSION['nivel_usuario'] == 'Admin') {
+            echo 'Administrador';
+        }
+
+        if ($_SESSION['nivel_usuario'] == 'Cliente') {
+            echo 'Cliente';
+        }
+
     }
 ?>
